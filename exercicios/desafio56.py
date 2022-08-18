2afcd3e651fd40213dadea8dba71e3a402bba76b
Lucas Henrique <lucas.henrique.andrade.ti@gmail.com>
2022-08-18 08:34:12 -0300

    Update desafio56.py

--- exercicios/desafio56.py
+++ exercicios/desafio56.py
@@ -2,18 +2,16 @@ from math import ceil
 media=0
 homem=''
 velho=0
-mulher=''
-for c in range(1,4):
+for c in range(1,5):
     nome=str(input('Nome: '))
     idade=int(input('Idade: '))
     sexo=str(input('''Quando o seu Gênero. Digite 'H' para Homem ou 'M' para Mulher: ''')).lower()
     if sexo=='h':
+        media += idade
         if idade > velho:
             velho = idade
         if idade == velho:
             homem=nome
 
-    #media += idade
-#print('A média de idade do grupo é de {} '.format(ceil(media/3)))
-print(velho)
-print(homem)
+print('A média de idade do grupo é de {} '.format(ceil(media/3)))
+print('O homem mais velho é o {} e ele tem {} anos!'.format(homem,velho))
