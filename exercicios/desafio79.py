7609a464fa605a222cc81782ace452618fb26dce
LucasHenriqueTi <lucas.henrique.andrade.ti@gmail.com>
2022-08-30 15:16:13 -0300

    estudospython

--- exercicios/desafio79.py
+++ exercicios/desafio79.py
@@ -1,11 +1,18 @@
+"""Crie um programa onde o usuário possa digitar vários valores numéricos e cadastre-os em uma lista.
+Caso o número já exista lá dentro, ele não será adicionado. No final, serão exibidos todos os valores únicos digitados,
+em ordem crescente. """
+
 lista = []
 while True:
-    lista.append(int(input('digite um número: ')))
-
+    n1 = (int(input('digite um numero: ')))
+    num = n1
+    if num in lista:
+        print('valor repetido')
+    else:
+        lista.append(num)
+        print('numero adicionado!')
     sair = str(input('quer continuar? [S/N] ')).upper().strip()[0]
     if sair == 'N':
         break
-    while True:
-        sair2 = str(input('opção invalida, tente novamente: ')).upper().strip()[0]
-        if sair2 == 'S':
-            break
+lista.sort()
+print(lista)
