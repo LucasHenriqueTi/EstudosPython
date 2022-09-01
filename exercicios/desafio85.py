0e6f2acab2cdcdfbb0174c6aae04a271a4e63b43
LucasHenriqueTi <lucas.henrique.andrade.ti@gmail.com>
2022-09-01 14:18:38 -0300

    Update desafio85.py

--- exercicios/desafio85.py
+++ exercicios/desafio85.py
@@ -1,5 +1,15 @@
-listap = []
-impar = []
-par = []
-for c in range(0, 4):
-    num = int(input(f'digite {c}º número: '))
+"""Crie um programa onde o usuário possa digitar sete valores numéricos e
+cadastre-os em uma lista única que mantenha separados os valores pares e ímpares. No final,
+mostre os valores pares e ímpares em ordem crescente."""
+
+listap = [[], []]
+for c in range(0, 7):
+    num = int(input(f'digite {c+1}º número: '))
+    if num % 2 == 0:
+        listap[0].append(num)
+    else:
+        listap[1].append(num)
+listap[0].sort()
+listap[1].sort()
+print(f'os números pares são {listap[0]}')
+print(f'os números ímpares são {listap[1]}')
