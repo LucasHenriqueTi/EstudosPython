d9c13e7d315b410514be34343ccc0d1c4351c0f5
LucasHenriqueTi <lucas.henrique.andrade.ti@gmail.com>
2022-09-01 14:33:27 -0300

    estudospython

---
+++ exercicios/desafio86.py
@@ -0,0 +1,9 @@
+matriz = [[], [], []]
+for p in range(0, 3):
+    matriz[0].append(int(input('digite um número: ')))
+for p in range(0, 3):
+    matriz[1].append(int(input('digite um número: ')))
+for p in range(0, 3):
+    matriz[2].append(int(input('digite um número: ')))
+
+print(matriz)
